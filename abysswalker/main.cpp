bd3dbe8a8cfcc7282629e9d890a961fe154ea4af
MadeinCanada <kristof.konig13@gmail.com>
2025-10-04 16:22:32 +0100

    Added basic item selection system

---
+++ abysswalker/main.cpp
@@ -0,0 +1,299 @@
+// abysswalker.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <conio.h>
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// Global Variables
+string textSeparator = "\n========================================\n\n";
+string titleDisplay[6] = {
+    "   _____ ___.                                        .__   __                 ",
+"  /  _  \\\\_ |__ ___.__. ______ ________  _  _______  |  | |  | __ ___________ ",
+" /  /_\\  \\| __ <   |  |/  ___//  ___/\\ \\/ \\/ /\\__  \\ |  | |  |/ // __ \\_  __ \\",
+"/    |    \\ \\_\\ \\___  |\\___ \\ \\___ \\  \\     /  / __ \\|  |_|    <\\  ___/|  | \\/ ",
+"\\____|__  /___  / ____/____  >____  >  \\/\\_/  (____  /____/__|_ \\\\___  >__|",
+"        \\/    \\/\\/         \\/     \\/               \\/          \\/    \\/       "
+};
+
+string setArea = "Deeproot Depths";
+string setKeepsake = "None";
+string setOldSoul = "None";
+bool gameStarted = false;
+
+map<int, map<string, string>> saveSlots;
+map<string, map<string, string>> keepsakesList;
+vector<vector<string>>  qPressInfo = {
+    { "View Itemlist", "Change Region", "Change Keepsake", "Change Old Soul", "Start", "Life Ring", "Fire Gem", "Black Firebombs", "Test Item" },
+    { "Items are the main way of getting stronger in ABYSSWALKER. There are many that can be unlocked by meeting specific conditions, allowing them to appear in-game.", "There are 3 regions to travel through in ABYSSWALKER. Each has different locations, enemies, bosses, and mechanics. To unlock the next region, you must achieve victory in the prior region.", "Keepsakes are items that you start with. They usually provide passive effects, but can sometimes be used in specific situations. More keepsakes can be unlocked by completing specific objectives.", "Old Souls are the powerful souls of heroes long gone. If one is selected, you will begin with different stats and items. Old Souls can be found during gameplay, unlocking them permanently.", "Select this option to begin the game! Make sure you have chosen your starting gear well.", "Begin with +3 HP.", "Consumable item. Use at a Blacksmith to add the Fire effect to your current weapon.", "Every other turn in combat, attack a second time, inflicting 2 damage. Triggers up to 3 times in one fight.", "Unknown." }
+};
+
+const string RED = "\033[0;31m";
+const string GREEN = "\033[0;32m";
+const string YELLOW = "\033[0;33m";
+const string BLUE = "\033[0;36m";
+const string RESET = "\033[0m";
+
+// Utility Functions
+string colourText(const string& text, const string& colour)
+{
+    return colour + text + RESET;
+}
+
+void clearScreen()
+{
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
+
+void displayTitle()
+{
+    for (string titleLine : titleDisplay) {
+        cout << titleLine << "\n";
+    }
+    cout << textSeparator;
+}
+
+int selectionIndexUpdate(int currentIndex, int arrayLength, char input)
+{
+    int newArrayLength = arrayLength - 1;
+    // char selectionIndexChange = _getch();
+
+    // W and S press will cycle through list of options
+    if (input == 's') {
+        if (currentIndex >= newArrayLength) { currentIndex = 0; }
+        else { ++currentIndex; }
+    }
+    else if (input == 'w') {
+        if (currentIndex <= 0) { currentIndex = newArrayLength; }
+        else { --currentIndex; }
+    }
+
+    return currentIndex;
+}
+
+void qPressCheck(string currentSelection)
+{
+    for (int i = 0; i < qPressInfo[0].size(); ++i) {
+        if (qPressInfo[0][i] == currentSelection) {
+            cout << "\n [INFO]: " << qPressInfo[1][i] << "\n";
+        }
+    }
+}
+
+string displayList(string listType)
+{
+    int selectionIndex = 0;
+    bool endDisplayList = false;
+    string selectedItem = "";
+
+    while (true) {
+        displayTitle();
+        for (int i = 0; i < qPressInfo[0].size(); ++i) {
+            if (qPressInfo[0][i] == listType) {
+                cout << qPressInfo[1][i] << "\n";
+            }
+        }
+        cout << textSeparator;
+
+        string currentLine = "";
+        string currentItem = "";
+        vector<string> displayList = {};
+        vector<string> fullList = {};
+
+        // Loop through each item in keepsakes.json, fill out the dictionary with keys and values
+        ifstream keepsakesFile("keepsakes.json");
+        while (getline(keepsakesFile, currentLine)) {
+            if (currentLine.empty()) continue;
+
+            if (currentLine.find("[") != string::npos) {
+                currentItem = currentLine.substr(1, currentLine.length()-2);
+                //cout << currentItem;
+                fullList.push_back(currentItem);
+            }
+
+            else if (currentItem != "" && currentLine.find(":") != string::npos) {
+                size_t colonPosition = currentLine.find(":");
+                string key = currentLine.substr(0, colonPosition);
+                string value = currentLine.substr(colonPosition + 2);
+                keepsakesList[currentItem][key] = value;
+            }
+
+        }
+        keepsakesFile.close();
+
+        // Loop through updated dictionary, display all unlocked items
+        for (string item : fullList) {
+            if (keepsakesList[item]["unlocked"] == "true") { displayList.push_back(item); }
+            else { displayList.push_back("???"); }
+        }
+        displayList.push_back("None");
+
+        string selectionValue = displayList[selectionIndex];
+        for (string selectionItem : displayList) {
+            if (selectionValue == selectionItem) {
+                cout << colourText(" > " + selectionItem, YELLOW) << "\n";
+            }
+            else {
+                cout << "   " << selectionItem << "\n";
+            }
+        }
+
+        while (true) {
+            char qCheck = _getch();
+            if (qCheck == 'q') {
+                qPressCheck(selectionValue);
+            }
+            else if (qCheck == 's' || qCheck == 'w') {
+                int newSelectionIndex = selectionIndexUpdate(selectionIndex, displayList.size(), qCheck);
+                selectionIndex = newSelectionIndex;
+                clearScreen();
+                break;
+            }
+            else if (qCheck == 'e') {
+                if (displayList[selectionIndex] != "???") {
+                    selectedItem = displayList[selectionIndex];
+                    endDisplayList = true;
+                    clearScreen();
+                    break;
+                }
+            }
+        }
+        if (endDisplayList) {
+            break;
+        }
+    }
+    return selectedItem;
+}
+
+// Main Loop Functions
+bool checkForSave()
+{
+    displayTitle();
+
+    //int selectedSave = -1;
+    string currentLine = "";
+    int currentSlot = -1;
+    int saveSlotArray[3] = { 0, 0, 0 };
+
+    // Find savefile.json (if possible), create the file if not present
+    ifstream saveFile("savefile.json");
+    if (!saveFile.is_open()) {
+        cout << "No save found. Creating new file.\n";
+        return false;
+    }
+
+    // Read through the file
+    while (getline(saveFile, currentLine)) {
+        //cout << currentLine + "\n";
+        if (currentLine.empty()) continue;
+
+        if (currentLine.find("[Slot") != string::npos) {
+            currentSlot = stoi(currentLine.substr(5, 1));
+            //saveSlots[currentSlot] = map < string, string >> "()";
+        }
+
+        // Use npos!!
+        // If ":" is in this line, find it and get the key + value for dictionary
+        else if (currentSlot != -1 && currentLine.find(":") != string::npos) {
+            size_t colonPosition = currentLine.find(":");
+            string key = currentLine.substr(0, colonPosition);
+            string value = currentLine.substr(colonPosition + 2);
+            saveSlots[currentSlot][key] = value;
+
+            // Mark save slot as not empty (for display)
+            if (saveSlotArray[currentSlot] == 0) {
+                saveSlotArray[currentSlot] = 1;
+            }
+        }
+    }
+
+    saveFile.close();
+
+    string slotName = "";
+    // Print saves to choose from
+    cout << "Enter the index of a save slot to continue.\n\n";
+    for (int i = 1; i < 4; i++) {
+        slotName = "Slot #" + to_string(i);
+        cout << colourText(slotName, YELLOW) << ":\n";
+
+        if (saveSlotArray[i-1] == 1) {
+            cout << " - " << saveSlots[i-1]["items"] << "/56 Items\n";
+            cout << " - " << saveSlots[i-1]["old_souls"] << "/6 Old Souls\n";
+        }
+        else {
+            cout << " Empty\n";
+        }
+    }
+    cout << textSeparator;
+
+    char selectedSave = _getch();
+    cout << "Loading save slot " << selectedSave << "...\n";
+
+    clearScreen();
+    return true;
+}
+
+void displayGamePreparations()
+{
+    string newlySelectedItem = "";
+    string selectionArray[] = { "View Itemlist", "Change Region", "Change Keepsake", "Change Old Soul", "Start" };
+    int selectionArrayLength = sizeof(selectionArray) / sizeof(selectionArray[0]);
+    int selectionIndex = 0;
+
+    while (true) {
+        displayTitle();
+        cout << " Prepare for your journey before starting. You can press Q for more information about the currently selected line at any time.\n\n";
+        cout << colourText(" Region: ", BLUE) << setArea << "\n";
+        cout << colourText(" Keepsake: ", BLUE) << setKeepsake << "\n";
+        cout << colourText(" Old Soul: ", BLUE) << setOldSoul << "\n";
+        cout << textSeparator;
+
+        string selectionValue = selectionArray[selectionIndex];
+        //cout << "  Change Region\n  Change Keepsake\n  Change Old Soul\n  Start\n";
+        for (string selectionItem : selectionArray) {
+            if (selectionValue == selectionItem) {
+                cout << colourText(" > " + selectionItem, YELLOW) << "\n";
+            }
+            else {
+                cout << "   " << selectionItem << "\n";
+            }
+        }
+
+        while (true) {
+            char qCheck = _getch();
+            if (qCheck == 'q') {
+                qPressCheck(selectionValue);
+            }
+            else if (qCheck == 's' || qCheck == 'w') {
+                int newSelectionIndex = selectionIndexUpdate(selectionIndex, selectionArrayLength, qCheck);
+                selectionIndex = newSelectionIndex;
+                clearScreen();
+                break;
+            }
+            else if (qCheck == 'e') {
+                clearScreen();
+                newlySelectedItem = displayList(selectionValue);
+                if (selectionValue == "Change Keepsake") { setKeepsake = newlySelectedItem; }
+                else if (selectionValue == "Change Old Soul") { setOldSoul = newlySelectedItem; }
+                else if (selectionValue == "Change Region") { setArea = newlySelectedItem; }
+                break;
+            }
+        }
+    }
+}
+
+// Game Loop
+int main()
+{
+    checkForSave();
+
+    displayGamePreparations();
+}
